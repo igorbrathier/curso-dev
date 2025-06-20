1056bf9cb9aff90ff2de9d38c43497a880ec3743
igorbrathier <igorbogonirathier2005@gmail.com>
2025-06-20 21:18:43 +0000

    adiciona scripts  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # curso-dev
+
 curso dev felipe deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
-export default function Home(){
-    return (
-    <h1>
-        commit teste+
-    </h1>
-    )
+export default function Home() {
+  return <h1>commit teste+</h1>;
 }

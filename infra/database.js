c81c510ebfa7a65eb5ed92d9781340849d91c2a3
igorbrathier <igorbogonirathier2005@gmail.com>
2025-10-28 21:50:31 +0000

    fix: ajusta configuração SSL e nome do banco

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   user: process.env.POSTGRES_USER,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  ssl: process.env.NODE.ENV == "development" ? false : true,
+  ssl: {
+    rejectUnauthorized: false,
+    sslmode: "require",
+  },
 
   // Configurações do pool
   max: 20, // máximo de conexões (padrão: 10)
